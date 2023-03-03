ae91272162236caca247a297a75f34dd16b1a2b4
Laura Caroline Ribeiro de Melo <laura@laura-melo.com>
2023-03-03 22:35:53 +0100

    Remove unnecessary content

--- src/settings/index.ts
+++
@@ -1,7 +0,0 @@
-import GoalTrackerSettings from "./settings.interface";
-
-const DEFAULT_SETTINGS: GoalTrackerSettings = {
-	mySetting: "default",
-};
-
-export default DEFAULT_SETTINGS;
--- src/settings/settings.interface.ts
+++
@@ -1,3 +0,0 @@
-export default interface GoalTrackerSettings {
-	mySetting: string;
-}
